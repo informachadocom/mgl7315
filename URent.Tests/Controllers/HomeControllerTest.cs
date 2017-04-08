784c9c56ec9d84763e92b88eff4a503bd3c75039
Marcos Muranaka <marcosmrk@gmail.com>
2017-04-07 21:24:15 -0400

    URent application
    Création des tests unitaires:
    - Génération de données des clients
    - Création du client
    - Modification du client
    - Authentification valide

--- URent.Tests/Controllers/HomeControllerTest.cs
+++ URent.Tests/Controllers/HomeControllerTest.cs
@@ -10,6 +10,7 @@ using URent.Models.Interfaces;
 using URent.Models.Manager;
 using URent.App_Start;
 using Ninject;
+using URent.Models.Model;
 
 namespace URent.Tests.Controllers
 {
@@ -28,14 +29,51 @@ namespace URent.Tests.Controllers
         {
             IKernel kernel = new StandardKernel();
             category = kernel.Get<CategoryManager>();
+            option = kernel.Get<OptionManager>();
+            client = kernel.Get<ClientManager>();
         }
 
         [TestMethod]
-        public void ListAllCategories()
+        public void GenerateDataClient()
         {
-            //var category = new CategoryManager();
-            var list = category.ListCategories();
-            Assert.IsTrue(list.Count > 0);
+            var result = client.Generate();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CreateOneClient()
+        {
+            var model = new Client();
+            model.FirstName = "URent";
+            model.Surname = "Surname";
+            model.Password = "password";
+            model.Email = "test@email.com";
+            var result = client.CreateUpdate(model);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ModifyOneClient()
+        {
+            var model = new Client();
+            model.ClientId = 5;
+            model.FirstName = "URent2";
+            model.Surname = "Surname2";
+            model.Password = "password2";
+            model.Email = "test2@email.com";
+            var result = client.CreateUpdate(model);
+            var obj = client.ListClient(5);
+            Assert.IsTrue(result && obj.Email == "test2@email.com");
+        }
+
+        [TestMethod]
+        public void CorrectAuthentificationUser()
+        {
+            var model = new Client();
+            model.Email = "test2@email.com";
+            model.Password = "password2";
+            var result = client.Authentification(model);
+            Assert.IsTrue(result.GetType() == typeof(Client) && result.ClientId == 5);
         }
     }
 }
