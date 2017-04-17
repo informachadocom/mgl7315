f377ca6e28c5d5d0d31433a29e79d3549c646ff4
FrederykMtl <frederic.simard@gmail.com>
2017-04-16 23:09:56 -0400

    Ajout interface generique

---
+++ URent.Models/Interfaces/IManager.cs
@@ -0,0 +1,12 @@
+﻿namespace URent.Models.Interfaces
+{
+    /// <summary>
+    /// Auteur: Frédéric Simard
+    /// Date: 16/04/2017
+    /// Description: Interface commune générique pour les manager
+    /// </summary>
+    public interface IManager
+    {
+        bool Generate();
+    }
+}
